0dcfe1c423cda6bfb31f947ed8b1f8b1587a0591
Andre Bellmann <andre.bellmann@huk-coburg.de>
2025-07-03 15:11:34 +0200

    Enhance reset button SVG icon: update design for improved clarity and visual appeal

--- src/components/online/VersionsChecker.svelte
+++ src/components/online/VersionsChecker.svelte
@@ -314,8 +314,9 @@
 		on:click={resetAll}
 		title="Alles zurücksetzen (Versionen + Umgebung)">
 		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-			<path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"/>
-			<path d="M3 3v5h5"/>
+			<path d="M3 6h18"/>
+			<path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/>
+			<path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/>
 		</svg>
 	</button>
 </div>
